5b0ffb1fb9e3a02545f57bcf71f593398a24f7f6
jian.xu <jian.xu@dena.com>
2018-07-24 11:07:29 +0800

    Add NavMesh

---
+++ NavMesh/Assets/Scripts/PlaceBuild.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlaceBuild : MonoBehaviour {
+    public GameObject builderPrefab;
+    private NavMeshSurface surface;
+
+	// Use this for initialization
+	void Start () {
+        surface = GetComponent<NavMeshSurface>();
+        surface.BuildNavMesh();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                GameObject go = GameObject.Instantiate(builderPrefab, hit.point + new Vector3(0.0f, 1f, 0.0f), Quaternion.identity);
+                go.transform.parent = this.transform;
+                surface.BuildNavMesh();
+            }
+             
+        }
+	}
+}
---
+++ NavMesh/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour 
+{
+    private NavMeshAgent agent;
+    public Transform target;
+    public float rotateSmoothing = 7.0f;
+    public float speed = 4f;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        //agent.destination = target.position; 
+        //agent.updatePosition = false;
+        //agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+
+        }
+
+        //agent.nextPosition = transform.position;
+        //Move();
+    }
+
+
+    private void Move()
+    {
+        if(agent.remainingDistance > 0.0001f)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(agent.desiredVelocity), rotateSmoothing);
+            transform.Translate(Vector3.forward * agent.speed * Time.deltaTime);
+        }
+        
+    }
+}
