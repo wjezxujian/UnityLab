c707063f696f183a31e90b53ee3199401fb2a4fc
jian.xu <jian.xu@dena.com>
2018-07-30 19:26:45 +0800

    Add DOWeen

---
+++ DOTWeen/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ButtonClick : MonoBehaviour {
+    public RectTransform planeTransform;
+
+    private bool isHide = true;
+    private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = planeTransform.localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Click()
+    {
+        if(isHide)
+        {
+            print("Click");
+            //让paneltransform从当前位置动画到(0, 0, 0)的位置 时间为1s(修改的世界坐标)
+            Tweener tweener = planeTransform.DOLocalMove(new Vector3(0, 0, 0), 0.3f); //默认动画播放完成销毁
+            //Tweener对象保存这个动画的信息 每次调用do类型的方法都会创建一个tweener对象，这个对象是dotweenr来管理
+            tweener.SetAutoKill(false);
+            isHide = false;
+        }
+        else
+        {
+            //planeTransform.DOLocalMove(startPos, 0.3f);
+            planeTransform.DOPlayBackwards(); //倒放
+            isHide = true;
+        }
+    }
+}
---
+++ DOTWeen/Assets/Scripts/GetStar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GetStar : MonoBehaviour {
+    public Transform targetCube;
+
+    public RectTransform taskPanelTransform;
+
+    public Vector3 myValue = new Vector3(0, 0, 0);
+
+    public float myValue2 = 0;
+	// Use this for initialization
+	void Start () {
+        //对变量做一个动画（通过插值的方式去修改一个值的变化）
+        //DOTween.To(() => myValue, x => myValue = x, new Vector3(10, 10, 10), 2);
+        DOTween.To(() => myValue, x => myValue = x, new Vector3(0, 0, 0), 2);
+
+        DOTween.To(() => myValue2, X => myValue2 = X, 10, 2);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        targetCube.position = myValue;
+        //taskPanelTransform.position = myValue;
+        taskPanelTransform.localPosition = myValue;
+
+    }
+}
