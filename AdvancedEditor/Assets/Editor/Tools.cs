dab0a746e692d664b8ddda9da743dd47f00488fb
wjezxujian <wjezxujian@gmail.com>
2018-08-01 09:09:44 +0800

    Add Survival Shooter

---
+++ AdvancedEditor/Assets/Editor/Tools.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+public class Tools {
+    //[MenuItem("Tools/test/test1")]
+    //static void Test()
+    //{
+    //    Debug.Log("Test");
+    //}
+
+    //[MenuItem("Tools/test/test2")]
+    //static void Test2()
+    //{
+    //    Debug.Log("Test2");
+    //}
+
+    [MenuItem("GameObject/mytool", false, 11)]
+    static void MyTool()
+    {
+        Debug.Log("GameObjectMyTool");
+    }
+
+    [MenuItem("Assets/mytool", false, 1000)]
+    static void AssetsMyTool()
+    {
+        Debug.Log("AssetsMyTool");
+    }
+
+    //每一个菜单栏的proority优先级默认为1000
+    //Proprity相差11就添加横线分组
+    [MenuItem("Tools/test1", false, 1)]
+    static void Test()
+    {
+        Debug.Log("Test");
+    }
+
+    [MenuItem("Tools/test2", false , 12)]
+    static void Test2()
+    {
+        Debug.Log("Test2");
+    }
+
+    [MenuItem("Tools/test3", false, 0)]
+    static void Test3()
+    {
+        Debug.Log("Test3");
+    }
+
+}
