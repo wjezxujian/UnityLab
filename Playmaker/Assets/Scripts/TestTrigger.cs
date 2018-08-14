cb654e7d78fc6cec2a752414a151952b594f044f
wjezxujian <wjezxujian@gmail.com>
2018-08-15 07:57:45 +0800

    update PlayMaker

---
+++ Playmaker/Assets/Scripts/TestTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Trigger Enter!");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Collision Enter!");
+    }
+}
