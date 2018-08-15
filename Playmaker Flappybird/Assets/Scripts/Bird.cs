4280c67f1a268e0e5f75d58ce488e96f3fa43f3f
wjezxujian <wjezxujian@gmail.com>
2018-08-15 23:05:07 +0800

    Add Flappybird Project

---
+++ Playmaker Flappybird/Assets/Scripts/Bird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void AddForce(Vector2 force)
+    {
+        Vector2 veloctity = rigidbody.velocity;
+        veloctity.y = 0;
+        rigidbody.velocity = veloctity;
+        rigidbody.AddForce(force);
+    }
+}
