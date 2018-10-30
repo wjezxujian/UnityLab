83933d050e759caa82325efc9e6d67e1564bbad7
wjezxujian <wjezxujian@gmail.com>
2018-10-30 08:19:29 +0800

    Add Builder Pattern

--- RedAlert/Assets/Scripts/Factory/Character/SoldierFactory.cs
+++ RedAlert/Assets/Scripts/Factory/Character/SoldierFactory.cs
@@ -9,57 +9,8 @@ class SoldierFactory : ICharacterFactory
     {
         ICharacter character = new T();
 
-        string name = "";
-        int maxHP = 0;
-        float moveSpeed = 0f;
-        string iconSprite = "";
-        string prefabName = "";
+        ICharacterBuilder builder = new SoldierBuilder(character, typeof(T), weaponType, spawnPosition, lv);-
 
-        System.Type t = typeof(T);
-        if(t == typeof(SoldierCaptain))
-        {
-            name = "上尉";
-            maxHP = 100;
-            moveSpeed = 3;
-            iconSprite = "CaptainIcon";
-            prefabName = "Soldier1";
-        }
-        else if (t == typeof(SoldierSergeant))
-        {
-            name = "中士";
-            maxHP = 90;
-            moveSpeed = 3;
-            iconSprite = "SergeantIcon";
-            prefabName = "Soldier2";
-        }
-        else if (t == typeof(SoldierRookie))
-        {
-            name = "中士";
-            maxHP = 80;
-            moveSpeed = 2.5f;
-            iconSprite = "RookieIcon";
-            prefabName = "Soldier3";
-        }
-        else
-        {
-            Debug.LogError("类型" + t + "不属于ISoldier，无法创建战士");
-            return null;
-        }
-
-        ICharacterAttr attr = new SoldierAttr(new SoldierAttrStrategy(), lv, name, maxHP, moveSpeed, iconSprite, prefabName);
-        character.attr = attr;
-
-        //创建角色游戏物体
-        //1.加载 2，实例化
-        GameObject characterGO = FactoryManager.assetFactory.LoadSoldier(prefabName);
-        characterGO.transform.position = spawnPosition;
-        character.gameObject = characterGO;
-
-
-        //添加武器
-        IWeapon weapon = FactoryManager.weaponFactory.CreateWeapon(weaponType);
-        character.weapon = weapon;
-
-        return character;
+        return CharacterBuilderDirector.Construct(builder);
     }
 }
